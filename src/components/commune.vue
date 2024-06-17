0cd85c54d4864abe8fdd22332bbb5f3d94c6968f
frankguoyr <2437474852@qq.com>
2024-06-17 11:57:07 +0800

    okk

---
+++ src/components/commune.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<el-container>
+			<el-header>commune</el-header>
+			<el-main>
+			</el-main>
+			<el-footer>
+			</el-footer>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	*{
+		font-size: 2vh;
+	}
+	.el-footer {
+	    background-color: aliceblue;
+	    color: black;
+	    text-align: center;
+		align-items: center;
+	    height:5%;
+		display:flex;
+		flex-direction: row;
+	  }
+	   .el-main {
+	      background-color: white;
+	      color: #333;
+	      text-align: center;
+	      height:90%;
+	    }
+</style>
